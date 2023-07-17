8e007b87d90a74e5b0a48145c283ce149d123d7a
Winston Giang <winstonjgiang@gmail.com>
2023-07-17 15:40:39 -0700

    update with trivia api

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ const CHATBOT_USER_OBJ = {
 
 export default function App() {
   const [messages, setMessages] = useState([]);
+  const [questionBank, setQBank] = useState({});
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     setMessages([
@@ -19,6 +21,7 @@ export default function App() {
         user: CHATBOT_USER_OBJ,
       },
     ]);
+    fetch5Questions();
   }, []);
 
   const addNewMessage = (newMessages) => {
@@ -40,70 +43,70 @@ export default function App() {
     ]);
   };
 
-  const questionBank = {
-    0: {
-      question: "Please enter 'ready' if you'd like to begin",
-      answer: "ready",
-      correct: "Ok heres your first question:\n",
-      wrong: "Please enter 'ready' if you'd like to begin. (wrong)",
-    },
-    1: {
-      question:
-        "What is the most abundant gas within our sun? Is it... \n 1. Helium \n 2. Hydrogen \n 3. Oxygen \n 4. Nitrogen \n Type the number of your answer!",
-      answer: "1",
-      correct: "Yay! Next Question",
-      wrong: "Boohooo... try again",
-    },
-    2: {
-      question:
-        "Is Winston a dog or cat person? \n 1. Dog \n 2. Cat \n 3. Sea Turtle \n 4. Monkey",
-      answer: "4",
-      correct: "Yay! Next Question",
-      wrong: "Boohooo... try again",
-    },
-    3: {
-      question:
-        "Is Phoenix a bird, a city, or the SEA coach? \n 1. Bird \n 2. City \n 3. Coach \n 4. Arizona",
-      answer: "4",
-      correct: "Yay! Next Question",
-      wrong: "Boohooo... try again",
-    },
-    4: {
-      question:
-        "What is Winston's favorite color? \n 1. Black \n 2. White \n 3. Blue \n 4. Red",
-      answer: "2",
-      correct: "Yay! Next Question",
-      wrong: "Boohooo... try again",
-    },
-    5: {
-      question:
-        "What is Jenna's middle name? \n 1. Kim \n 2. Emily \n 3. Julia \n 4. Cho",
-      answer: "2",
-      correct: "Yay! Final Question",
-      wrong: "Boohooo... try again",
-    },
-    6: {
-      question:
-        "What is Winston's current favorite artist? \n 1. A Boogie wit a Hoodie \n 2. The Weeknd \n 3. Drake \n 4. Don Toliver",
-      answer: "1",
-      correct: "wow nice!",
-      wrong: "Boohooo... try again",
-    },
-  };
+  function scramble(array) {
+    var i = array.length;
+    if (i == 0) return false;
+    while (--i) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tempi = array[i];
+      var tempj = array[j];
+      array[i] = tempj;
+      array[j] = tempi;
+    }
+  }
 
-  const [index, setIndex] = useState(0);
-  var bool = false;
+  function fetch5Questions() {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    const firstQuestion = {
+      correctAnswer: "ready",
+    };
+
+    fetch(
+      "https://the-trivia-api.com/v2/questions?limit=10&categories=general_knowledge&difficulties=easy",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        result.unshift(firstQuestion);
+        setQBank(result);
+      })
+      .catch((error) => console.log("error", error));
+  }
 
   const respondToUser = (userMessages) => {
     console.log("User message text:", userMessages[0].text);
 
-    if (index < 7) {
-      if (userMessages[0].text.toLowerCase() === questionBank[index].answer) {
-        addBotMessage(questionBank[index].correct);
-        addBotMessage(questionBank[index + 1].question);
+    if (index < 10) {
+      if (
+        userMessages[0].text.toLowerCase() ==
+        questionBank[index].correctAnswer.toLowerCase()
+      ) {
+        addBotMessage("Congrats! You got it right!!");
+        const answers = [
+          questionBank[index + 1].correctAnswer,
+          questionBank[index + 1].incorrectAnswers[0],
+          questionBank[index + 1].incorrectAnswers[1],
+          questionBank[index + 1].incorrectAnswers[2],
+        ];
+        scramble(answers);
+        question =
+          questionBank[index + 1].question.text +
+          "\n" +
+          answers[0] +
+          "\n" +
+          answers[1] +
+          "\n" +
+          answers[2] +
+          "\n" +
+          answers[3];
+        addBotMessage(question);
         setIndex(index + 1);
       } else {
-        addBotMessage(questionBank[index].wrong);
+        addBotMessage("Wrong answer.. try again!");
       }
     }
   };
